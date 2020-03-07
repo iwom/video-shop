1987ef629c1b706e66abd11ba02179eb100a25e7
iwom <17498068+iwom@users.noreply.github.com>
2020-03-07 14:05:37 +0100

    Spring boilerplate

---
+++ service/src/main/kotlin/com/example/videostore/VideostoreApplication.kt
@@ -0,0 +1,12 @@
+package com.example.videostore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class VideostoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<VideostoreApplication>(*args)
+    println("Hello Video Store")
+}
---
+++ service/src/test/kotlin/com/example/videostore/VideostoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.videostore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VideostoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
