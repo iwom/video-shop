7e45da794444ead21a5758103e13fb34034461b8
Łukasz Pięta <pieta.lukasz98@gmail.com>
2020-03-14 20:21:44 +0100

    Encrypt admin password

--- service/src/main/resources/db/migration/V1_1_0__create_users.sql
+++ service/src/main/resources/db/migration/V1_1_0__create_users.sql
@@ -1,6 +1,6 @@
 create table users
 (
-    id     uuid not null
+    id       uuid not null
         constraint users_pkey
             primary key,
     email    varchar(255) unique,
@@ -8,5 +8,7 @@ create table users
     role     varchar(255)
 );
 
+-- using BCrypt for password
 insert into users (id, email, password, role)
-values ('7791d5b2-a257-49c9-bb2b-384362fe83c7', 'admin@take.com', 'Admin12345', 'ADMIN');
+values ('7791d5b2-a257-49c9-bb2b-384362fe83c7', 'admin@take.com',
+        '$2a$10$ZOo/aqLvylvQwGyEsWu4dugpJTXnFaii9D2ui6helq/TsYjDwFJDe', 'ADMIN');
