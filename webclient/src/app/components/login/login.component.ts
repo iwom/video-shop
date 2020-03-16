5f552b57b2b8f8fa7614c2b2cfc0871da9a68abc
iwom <17498068+iwom@users.noreply.github.com>
2020-03-16 22:03:58 +0100

    Signup integration

--- webclient/src/app/components/login/login.component.ts
+++ webclient/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
+import { TokenStorageService } from "../../services/token.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loggedIn = false;
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private tokenStorageService: TokenStorageService
+  ) { }
 
   ngOnInit(): void {
+    if (this.tokenStorageService.getToken()) {
+      this.loggedIn = true;
+    }
   }
 
 }
--- webclient/src/app/services/auth.service.ts
+++ webclient/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiProvider } from "./api.provider";
+import {User, UserCredentials} from "../models/user";
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -14,15 +15,15 @@ export class AuthService {
 
   constructor(private http: HttpClient, private api: ApiProvider) { }
 
-  login(credentials): Observable<any> {
-    return this.http.post(this.api.go().login(), {
-      username: credentials.username,
-      password: credentials.password
+  login(userCredentials: UserCredentials): Observable<any> {
+    return this.http.post(this.api.go().signIn(), {
+      username: userCredentials.username,
+      password: userCredentials.password
     }, httpOptions);
   }
 
-  register(user): Observable<any> {
-    return this.http.post(this.api.go().signup(), {
+  register(user: User): Observable<any> {
+    return this.http.post(this.api.go().signUp(), {
       username: user.username,
       email: user.email,
       password: user.password
