9674573762efdaf42fe782598f409366b370521e
Łukasz Pięta <pieta.lukasz98@gmail.com>
2020-03-29 20:59:31 +0200

    Model and repository created

---
+++ service/src/main/kotlin/com/example/videostore/movie/MovieRepository.kt
@@ -0,0 +1,14 @@
+package com.example.videostore.movie
+
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.Repository
+import java.util.*
+
+interface MovieRepository : Repository<Movie, UUID> {
+
+    @Query(value = "SELECT * from movie offset ?1 limit ?2", nativeQuery = true)
+    fun findAllWith(offset: Int, limit: Int): List<Movie>
+    fun findAll(): List<Movie>
+
+    fun save(movie: Movie): Movie
+}
---
+++ service/src/main/kotlin/com/example/videostore/movie/inventory/Inventory.kt
@@ -0,0 +1,25 @@
+package com.example.videostore.movie.inventory
+
+import com.example.videostore.movie.Movie
+import java.io.Serializable
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@IdClass(InventoryToMovie::class)
+data class Inventory(
+    @Id
+    @Column(columnDefinition = "uuid", updatable = false)
+    val id: UUID = UUID.randomUUID(),
+
+    val value: Int = 0,
+
+    @Id
+    @OneToOne
+    val movie: Movie = Movie()
+)
+
+class InventoryToMovie(
+    val id: UUID = UUID.randomUUID(),
+    val movie: UUID = UUID.randomUUID()
+) : Serializable
