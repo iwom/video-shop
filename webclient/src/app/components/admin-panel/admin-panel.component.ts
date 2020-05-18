c06ae8de4fc99b4e9144c63a78fffac0e4dafd55
iwom <17498068+iwom@users.noreply.github.com>
2020-05-18 21:03:39 +0200

    Admin Panel, autocomplete title

--- webclient/src/app/components/admin-panel/admin-panel.component.ts
+++ webclient/src/app/components/admin-panel/admin-panel.component.ts
@@ -1,43 +1,58 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {MovieService} from "../../services/movie.service";
 import {debounceTime, switchMap} from "rxjs/operators";
-import {CartDialogComponent} from "../cart-dialog/cart-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
 import {AdminDialogComponent} from "../admin-dialog/admin-dialog.component";
 import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
+import {Movie} from "../../models/movie";
 
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
   styleUrls: ['./admin-panel.component.css']
 })
-export class AdminPanelComponent implements OnInit {
+export class AdminPanelComponent implements OnInit, OnDestroy {
   movieForm = new FormGroup({
     title: new FormControl(''),
     amount: new FormControl(0),
     price: new FormControl(0.00)
   })
   options = [];
-  movie = null;
+  movie: Movie = null;
+  omdbSubsription: Subscription = null;
 
   constructor(
     private dialog: MatDialog,
     private router: Router,
     private movieService: MovieService
   ) {
+    const state = this.router.getCurrentNavigation().extras.state
+    if (state) {
+      const title = state['movie']
+      this.movieForm.controls['title'].setValue(title)
+      this.movieService.getByTitle(title).subscribe(movie => {
+        this.movie = movie;
+        this.options = [movie.title];
+      })
+    }
   }
 
   ngOnInit(): void {
-    this.movieForm.controls['title'].valueChanges.pipe(
+    this.omdbSubsription = this.movieForm.controls['title'].valueChanges.pipe(
       debounceTime(500),
       switchMap(title => this.movieService.getByTitle(title))
     ).subscribe(
       movie => {
         this.movie = movie;
-        this.options = [movie.title]
+        this.options = [movie.title];
       }
     )
+  }
+
+  ngOnDestroy() {
+    this.omdbSubsription.unsubscribe();
   }
 
   onSubmit() {
